db408d34a090d6e72816962805328e73d8081ec2
Rokunuzzaman Bhuiya <rokunuzzamanbhuiya@gmail.com>
2024-07-08 21:18:55 +0600

    some update

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a HTML Website.
+Website Name: Grindelwald - Welcome To Our Cosy Cabin In The Woods
+
---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+/* Theme Name: Grindelwald - Welcome To Our Cosy Cabin In The Woods
+   Author: Rokunuzzaman Bhuiya
+   Version: 1.0.0
+   File Description: Main JS file of the template
+*/
+
+"use strict";
+/*=============================================
+	=    		Preloader			      =
+=============================================*/
+window.addEventListener("load", function () {
+  document.querySelector("body").classList.add("loaded");
+});
